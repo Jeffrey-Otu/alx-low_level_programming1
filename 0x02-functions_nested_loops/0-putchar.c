f24c2bd379382911c79ce50fac217067dd7c42b7
Jeffrey-Otu <otujeffrey02@gmail.com>
2022-12-20 15:40:54 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <main.h>
+#include <unistd.h>
+
+/**
+ * main - print _putchar
+ * Return: 0 Always
+ */
+
+int main(void)
+
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
+
+
