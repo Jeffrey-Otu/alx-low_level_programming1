3653366d0734102a4061ceb6e87e2343b18a05c8
Jeffrey-Otu <otujeffrey02@gmail.com>
2022-12-31 21:33:29 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,11 @@
-#include <main.h>
-#include <unistd.h>
-<<<<<<< HEAD
-
+#include "main.h"
 /**
  * main - print _putchar
- * Return: 0 Always
+ * is written by mbah
+ * Return: 0
  */
 
 int main(void)
-
 {
 _putchar('_');
 _putchar('p');
@@ -21,25 +18,3 @@ _putchar('r');
 _putchar('\n');
 return (0);
 }
-
-
-=======
-/**
- * main - prints putchar
- * Return: 0 Always
- */
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-return (0);
-}
->>>>>>> ca4aaadef6ba06ea7604ab7ebb79a7cfaf6e576b
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,21 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
-<<<<<<< HEAD
-int _putchar(char c)
+#ifndef main_h
+#define main_h
 
-#endif
-=======
-
+int _putchar(char)
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int abs(int);
+int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-int _putchar(char c);
->>>>>>> ca4aaadef6ba06ea7604ab7ebb79a7cfaf6e576b
+int _print_times_table(int n);
+
+#endif
