a5e26e8cc35d1c88c80afc429886fd68d4d2c731
Jeffrey-Otu <otujeffrey02@gmail.com>
2023-01-04 16:33:07 -0800

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,14 +4,15 @@
 /**
  * File: main.h
  * Auth: otu jeffrey
- * description: Header file containing prototypes for all functions used in the 0x06-pointers_ arrays_strings directory.
+ * description: Header file containing prototypes for all functions
+ * used in the 0x06-pointers_ arrays_strings directory.
  */
 
 #include <stddef.h>
 
 int _putchar(char c);
 void *_memset(void *s, int c, size_t n);
-char *_memcoy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
