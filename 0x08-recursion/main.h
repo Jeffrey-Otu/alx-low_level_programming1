aa1b5fe99ad9cf912f02076e2cd2ca778ce558b7
Jeffrey-Otu <otujeffrey02@gmail.com>
2023-01-05 18:52:24 -0800

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,12 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * File: main.h
- * Auth: Otu Jeffrey
- * Desc: Header file containing
- * prototypes for all functions
- * used in the 0x07-recursion directory.
- */
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
