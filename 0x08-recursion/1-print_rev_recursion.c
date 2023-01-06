50968bfa3e3c36d351d1566aab98c175cb5ed5ad
Jeffrey-Otu <otujeffrey02@gmail.com>
2023-01-05 19:47:31 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,11 +2,14 @@
  * File: 1-print_rev_recursion.c
  * Auth: Jeffrey Otu
  */
+
 #include "main.h"
+
 /*
  * _print_rev_recursion - Prints a string in reverse.
  * @s: The string to be printed
  */
+
 void _print_rev_recursion(char *s)
 {
 	if (*s)
