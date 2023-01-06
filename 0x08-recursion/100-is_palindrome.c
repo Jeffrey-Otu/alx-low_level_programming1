297bb782a7419ab4cc275375879c3e7bdbff5c99
Jeffrey-Otu <otujeffrey02@gmail.com>
2023-01-05 21:24:27 -0800

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,28 @@
+/*
+ * File: 100-is_palindrome.c
+ * Auth: Jeffrey-Otu
+ */
+
+#include "main.h"
+
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
+
+/**
+ * find_strlen - Returns the length of a string.
+ * @s: The string to be measured.
+ * Return: The length of the string.
+ */
+int find_strlen(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
+
+	return (len);
+}
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,5 @@
 /*
- * File: 6-is_prime_number.c
+ * File 6-is_prime_number.c
  * Auth: Jeffrey-Otu
  */
 
@@ -10,13 +10,13 @@ int is_prime_number(int n);
 
 /**
  * is_divisible - Checks if a number is divisible.
+ * int is_prime_number(int n);
  * @num: The number to be checked.
  * @div: The divisor.
  * Return: If the number is divisible - 0.
  * If the number is not divisible - 1.
  */
-
-int is_divisible(int num, int div)
+int is_divisible(int n)
 {
 	if (num % div == 0)
 		return (0);
