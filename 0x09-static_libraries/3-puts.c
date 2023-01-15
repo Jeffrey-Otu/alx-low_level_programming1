ef339fdd186ac0d162936ce50b1d56eda71ecf9f
Jeffrey-Otu <otujeffrey02@gmail.com>
2023-01-14 19:59:55 -0800

    0x09

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include<stdio.h>
+
+void _puts(char *str)
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\nProgramming is like building a multilingual puzzle");
+
+	return (0);
+}
